674f41521eea86257444fcc09db04f43261b59d5
mathboylinlin <mathboylinlin@gmail.com>
2026-03-01 20:56:30 +0800

    add dynamic version support

--- ai4plasma/__init__.py
+++ ai4plasma/__init__.py
@@ -6,6 +6,11 @@ __all__ = [
     "plasma"
 ]
 
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown version"
+
 from . import core
 from . import utils
 from . import piml
